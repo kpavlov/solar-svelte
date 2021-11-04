9ba723c837f04b3a4d1a83ffee34f6f5c4bd844c
kpavlov <kpavlov@users.noreply.github.com>
2021-11-04 18:58:54 +0200

    github pages

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import glslify from 'rollup-plugin-glslify';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    base: '/solar-svelte/',
     plugins: [
         glslify(),
         svelte()
